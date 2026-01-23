44256024ec786f008c6b73a45539b7ce587e8e04
Khoon <kharieladignon01@gmail.com>
2026-01-23 23:21:06 +0800

    Add RBAC

--- Backend/src/router/userRoutes.js
+++ Backend/src/router/userRoutes.js
@@ -11,12 +11,30 @@ import {createUser,
         getAllUser} from '../controller/userController.js'
 import { registerSchema } from "../validation/user.validation.js";
 import { loginSchema } from "../validation/auth.validation.js";
+import { restrictTo } from "../middleware/authorize.js";
 
 const router = express.Router();
 
-router.post("/register", validate(registerSchema), createUser);
-router.post("/login", validate(loginSchema), loginLimiter, validateUser);
-router.get("/profile", protect, getProfile);
-router.get("/users", protect, isAdmin, getAllUser)
+router.post(
+        "/register", 
+        validate(registerSchema), 
+        createUser);
+
+router.post(
+        "/login", 
+        validate(loginSchema), 
+        loginLimiter, 
+        validateUser);
+
+router.get(
+        "/profile", 
+        protect, 
+        getProfile);
+
+router.get(
+        "/users", 
+        protect, 
+        restrictTo("admin"), 
+        getAllUser);
 
 export default router
